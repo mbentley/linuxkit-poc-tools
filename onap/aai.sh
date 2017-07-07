e2b7ff343506ae75d00c8b864a01567d3e2a908f
Matt Bentley <mbentley@mbentley.net>
2017-07-07 15:16:33 -0400

    Added aai; fixed tabs
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

---
+++ onap/aai.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+set -e
+
+remove() {
+  echo -e "\nKilling and removing containers..."
+  #shellcheck disable=2046
+  docker kill $(docker ps -f label=app=aai -qa) || true
+  #shellcheck disable=2046
+  docker rm $(docker ps -f label=app=aai -qa) || true
+
+  echo -e "\nRemoving volumes..."
+  #shellcheck disable=2046
+  docker volume rm $(docker volume ls -f label=app=aai -q) || true
+
+  echo -e "\nRemoving networks..."
+  #shellcheck disable=2046
+  docker network rm $(docker network ls -f label=app=aai -q) || true
+}
+
+launch() {
+  # aai
+  echo -e "\nCreating network..."
+  docker network create --label app=aai --driver bridge onap-aai
+
+  echo -e "\nCreating volumes..."
+  docker volume create --label app=aai --driver local aai-logroot
+
+  ## hbase
+  echo -e "\nLaunching hbase..."
+  docker run -d --name hbase \
+    --label app=aai \
+    --net onap-aai \
+    -p 8020:8020 \
+    dtr.att.dckr.org/onap/aai-hbase-1.2.3:latest
+
+  ## aai
+  echo -e "\nLaunching aai-service..."
+  docker run -d --name aai-service \
+    --label app=aai \
+    --net onap-aai \
+    -p 8080:8080 \
+    -p 8443:8443 \
+    -e AAI_REPO_PATH=r/aai \
+    -e AAI_CHEF_ENV=simpledemo \
+    -e AAI_CHEF_LOC=/var/chef/aai-data/environments \
+    -e docker_gitbranch=release-1.0.0 \
+    -e DEBIAN_FRONTEND=noninteractive \
+    -e JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 \
+    -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/aai/etc/ssl/certs:/etc/ssl/certs \
+    -v aai-logroot:/opt/aai/logroot \
+    -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/aai/aai-config:/var/chef/aai-config \
+    -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/aai/aai-data:/var/chef/aai-data \
+    dtr.att.dckr.org/onap/ajsc-aai:1.0-STAGING-latest
+    #-v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/aai/opt/aai/logroot:/opt/aai/logroot \
+
+  ## model-loader
+  echo -e "\nLaunching model-loader..."
+  docker run -d --name model-loader \
+    --label app=aai \
+    --net onap-aai \
+    -p 8080:8080 \
+    -p 8443:8443 \
+    -e DISTR_CLIENT_ASDC_ADDRESS=sdc-be.onap-sdc:8443 \
+    -e DISTR_CLIENT_ENVIRONMENT_NAME=OBF:1ks51l8d1o3i1pcc1r2r1e211r391kls1pyj1z7u1njf1lx51go21hnj1y0k1mli1sop1k8o1j651vu91mxw1vun1mze1vv11j8x1k5i1sp11mjc1y161hlr1gm41m111nkj1z781pw31kku1r4p1e391r571pbm1o741l4x1ksp \
+    -e APP_SERVER_BASE_URL=https://aai-service.onap-aai:8443 \
+    -e APP_SERVER_KEYSTORE_PASSWORD=OBF:1i9a1u2a1unz1lr61wn51wn11lss1unz1u301i6o \
+    -e APP_SERVER_AUTH_USER=ModelLoader \
+    -e APP_SERVER_AUTH_PASSWORD=OBF:1qvu1v2h1sov1sar1wfw1j7j1wg21saj1sov1v1x1qxw \
+    dtr.att.dckr.org/onap/model-loader:1.0-STAGING-latest
+}
+
+main() {
+  case $1 in
+    launch|remove)
+      ${1}
+      ;;
+    *)
+      echo "Usage: $0 {launch|remove}"
+      exit 1
+      ;;
+  esac
+}
+
+main "${@}"
--- onap/sdc.sh
+++ onap/sdc.sh
@@ -3,123 +3,123 @@
 set -e
 
 remove() {
-	echo -e "\nKilling and removing containers..."
-        #shellcheck disable=2046
-	docker kill $(docker ps -f label=app=sdc -qa) || true
-        #shellcheck disable=2046
-	docker rm $(docker ps -f label=app=sdc -qa) || true
+  echo -e "\nKilling and removing containers..."
+  #shellcheck disable=2046
+  docker kill $(docker ps -f label=app=sdc -qa) || true
+  #shellcheck disable=2046
+  docker rm $(docker ps -f label=app=sdc -qa) || true
 
-	echo -e "\nRemoving volumes..."
-        #shellcheck disable=2046
-	docker volume rm $(docker volume ls -f label=app=sdc -q) || true
+  echo -e "\nRemoving volumes..."
+  #shellcheck disable=2046
+  docker volume rm $(docker volume ls -f label=app=sdc -q) || true
 
-	echo -e "\nRemoving networks..."
-        #shellcheck disable=2046
-	docker network rm $(docker network ls -f label=app=sdc -q) || true
+  echo -e "\nRemoving networks..."
+  #shellcheck disable=2046
+  docker network rm $(docker network ls -f label=app=sdc -q) || true
 
 }
 
 launch() {
-	# sdc
-	echo -e "\nCreating network..."
-	docker network create --label app=sdc --driver bridge sdc
+  # sdc
+  echo -e "\nCreating network..."
+  docker network create --label app=sdc --driver bridge onap-sdc
 
-	echo -e "\nCreating volumes..."
-	docker volume create --label app=sdc --driver local sdc-es
-	docker volume create --label app=sdc --driver local sdc-logs
+  echo -e "\nCreating volumes..."
+  docker volume create --label app=sdc --driver local sdc-es
+  docker volume create --label app=sdc --driver local sdc-logs
 
-	## sdc-es
-	echo -e "\nLaunching sdc-es..."
-	docker run -d --name sdc-es \
-	  --label app=sdc \
-	  --net sdc \
-	  -p 9200:9200 \
-	  -p 9300:9300 \
-	  -e ENVNAME=AUTO \
-	  -e HOST_IP=172.31.4.207 \
-	  -e ES_HEAP_SIZE=1024M \
-	  -v /etc/localtime:/etc/localtime \
-	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
-	  -v sdc-es:/usr/share/elasticsearch/data \
-	  -v sdc-logs:/var/lib/jetty/logs \
-	  dtr.att.dckr.org/onap/sdc-elasticsearch:1.0-STAGING-latest
+  ## sdc-es
+  echo -e "\nLaunching sdc-es..."
+  docker run -d --name sdc-es \
+    --label app=sdc \
+    --net onap-sdc \
+    -p 9200:9200 \
+    -p 9300:9300 \
+    -e ENVNAME=AUTO \
+    -e HOST_IP=172.31.4.207 \
+    -e ES_HEAP_SIZE=1024M \
+    -v /etc/localtime:/etc/localtime \
+    -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
+    -v sdc-es:/usr/share/elasticsearch/data \
+    -v sdc-logs:/var/lib/jetty/logs \
+    dtr.att.dckr.org/onap/sdc-elasticsearch:1.0-STAGING-latest
 
-	## sdc-cs
-	echo -e "\nLaunching sdc-cs..."
-	docker run -d --name sdc-cs \
-	  --label app=sdc \
-	  --net sdc \
-	  -p 9042:9042 \
-	  -p 9160:9160 \
-	  -e ENVNAME=AUTO \
-	  -e HOST_IP=172.31.4.207 \
-	  -e ES_HEAP_SIZE=1024M \
-	  -v /etc/localtime:/etc/localtime \
-	  -v /dockerdata-nfs/onapdemo/sdc/sdc-cs/CS:/var/lib/cassandra \
-	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
-	  -v sdc-logs:/var/lib/jetty/logs \
-	  dtr.att.dckr.org/onap/sdc-cassandra:1.0-STAGING-latest
+  ## sdc-cs
+  echo -e "\nLaunching sdc-cs..."
+  docker run -d --name sdc-cs \
+    --label app=sdc \
+    --net onap-sdc \
+    -p 9042:9042 \
+    -p 9160:9160 \
+    -e ENVNAME=AUTO \
+    -e HOST_IP=172.31.4.207 \
+    -e ES_HEAP_SIZE=1024M \
+    -v /etc/localtime:/etc/localtime \
+    -v /dockerdata-nfs/onapdemo/sdc/sdc-cs/CS:/var/lib/cassandra \
+    -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
+    -v sdc-logs:/var/lib/jetty/logs \
+    dtr.att.dckr.org/onap/sdc-cassandra:1.0-STAGING-latest
 
-	## sdc-kb
-	echo -e "\nLaunching sdc-kb..."
-	docker run -d --name sdc-kb \
-	  --label app=sdc \
-	  --net sdc \
-	  -p 5601:5601 \
-	  -e ENVNAME=AUTO \
-	  -e ELASTICSEARCH_URL=http://sdc-es:9200 \
-	  -v /etc/localtime:/etc/localtime \
-	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
-	  -v sdc-logs:/var/lib/jetty/logs \
-	  dtr.att.dckr.org/onap/sdc-kibana:1.0-STAGING-latest
+  ## sdc-kb
+  echo -e "\nLaunching sdc-kb..."
+  docker run -d --name sdc-kb \
+    --label app=sdc \
+    --net onap-sdc \
+    -p 5601:5601 \
+    -e ENVNAME=AUTO \
+    -e ELASTICSEARCH_URL=http://sdc-es:9200 \
+    -v /etc/localtime:/etc/localtime \
+    -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
+    -v sdc-logs:/var/lib/jetty/logs \
+    dtr.att.dckr.org/onap/sdc-kibana:1.0-STAGING-latest
 
-	## sdc-fe
-	echo -e "\nLaunching sdc-fe..."
-	docker run -d --name sdc-fe \
-	  --label app=sdc \
-	  --net sdc \
-	  -p 8181:8181 \
-	  -p 9443:9443 \
-	  -e ENVNAME=AUTO \
-	  -e HOST_IP=172.31.4.207 \
-	  -v /etc/localtime:/etc/localtime \
-	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
-	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/jetty/keystore:/var/lib/jetty/etc/keystore \
-	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/sdc-fe/FE_2_setup_configuration.rb:/root/chef-solo/cookbooks/sdc-catalog-fe/recipes/FE_2_setup_configuration.rb \
-	  -v sdc-es:/usr/share/elasticsearch/data \
-	  -v sdc-logs:/var/lib/jetty/logs \
-	  dtr.att.dckr.org/onap/sdc-frontend:1.0-STAGING-latest
+  ## sdc-fe
+  echo -e "\nLaunching sdc-fe..."
+  docker run -d --name sdc-fe \
+    --label app=sdc \
+    --net onap-sdc \
+    -p 8181:8181 \
+    -p 9443:9443 \
+    -e ENVNAME=AUTO \
+    -e HOST_IP=172.31.4.207 \
+    -v /etc/localtime:/etc/localtime \
+    -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
+    -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/jetty/keystore:/var/lib/jetty/etc/keystore \
+    -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/sdc-fe/FE_2_setup_configuration.rb:/root/chef-solo/cookbooks/sdc-catalog-fe/recipes/FE_2_setup_configuration.rb \
+    -v sdc-es:/usr/share/elasticsearch/data \
+    -v sdc-logs:/var/lib/jetty/logs \
+    dtr.att.dckr.org/onap/sdc-frontend:1.0-STAGING-latest
 
-	#echo "Waiting for services to initialize (this is a sad hack)..."
-	#sleep 30
+  #echo "Waiting for services to initialize (this is a sad hack)..."
+  #sleep 30
 
-	## sdc-be
-	echo -e "\nLaunching sdc-be..."
-	docker run -d --name sdc-be \
-	  --label app=sdc \
-	  --net sdc \
-	  -p 8080:8080 \
-	  -p 8443:8443 \
-	  -e ENVNAME=AUTO \
-	  -e HOST_IP=172.31.4.207 \
-	  -v /etc/localtime:/etc/localtime \
-	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
-	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/jetty/keystore:/var/lib/jetty/etc/keystore \
-	  -v sdc-es:/usr/share/elasticsearch/data \
-	  -v sdc-logs:/var/lib/jetty/logs \
-	  dtr.att.dckr.org/onap/sdc-backend:1.0-STAGING-latest
+  ## sdc-be
+  echo -e "\nLaunching sdc-be..."
+  docker run -d --name sdc-be \
+    --label app=sdc \
+    --net onap-sdc \
+    -p 8080:8080 \
+    -p 8443:8443 \
+    -e ENVNAME=AUTO \
+    -e HOST_IP=172.31.4.207 \
+    -v /etc/localtime:/etc/localtime \
+    -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
+    -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/jetty/keystore:/var/lib/jetty/etc/keystore \
+    -v sdc-es:/usr/share/elasticsearch/data \
+    -v sdc-logs:/var/lib/jetty/logs \
+    dtr.att.dckr.org/onap/sdc-backend:1.0-STAGING-latest
 }
 
 main() {
-	case $1 in
-		launch|remove)
-			${1}
-			;;
-		*)
-			echo "Usage: $0 {launch|remove}"
-			exit 1
-			;;
-	esac
+  case $1 in
+    launch|remove)
+      ${1}
+      ;;
+    *)
+      echo "Usage: $0 {launch|remove}"
+      exit 1
+      ;;
+  esac
 }
 
 main "${@}"
