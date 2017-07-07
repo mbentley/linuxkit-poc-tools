92090c7484c4b9cdadc354a33e2baec2702b06bf
Matt Bentley <mbentley@mbentley.net>
2017-07-07 16:24:13 -0400

    Fixed env vars
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- onap/aai.sh
+++ onap/aai.sh
@@ -65,7 +65,9 @@ launch() {
     -p 8080 \
     -p 8443 \
     -e DISTR_CLIENT_ASDC_ADDRESS=sdc-be.onap-sdc:8443 \
-    -e DISTR_CLIENT_ENVIRONMENT_NAME=OBF:1ks51l8d1o3i1pcc1r2r1e211r391kls1pyj1z7u1njf1lx51go21hnj1y0k1mli1sop1k8o1j651vu91mxw1vun1mze1vv11j8x1k5i1sp11mjc1y161hlr1gm41m111nkj1z781pw31kku1r4p1e391r571pbm1o741l4x1ksp \
+    -e DISTR_CLIENT_ENVIRONMENT_NAME=AUTO \
+    -e DISTR_CLIENT_USER=aai \
+    -e DISTR_CLIENT_PASSWORD=OBF:1ks51l8d1o3i1pcc1r2r1e211r391kls1pyj1z7u1njf1lx51go21hnj1y0k1mli1sop1k8o1j651vu91mxw1vun1mze1vv11j8x1k5i1sp11mjc1y161hlr1gm41m111nkj1z781pw31kku1r4p1e391r571pbm1o741l4x1ksp \
     -e APP_SERVER_BASE_URL=https://aai-service.onap-aai:8443 \
     -e APP_SERVER_KEYSTORE_PASSWORD=OBF:1i9a1u2a1unz1lr61wn51wn11lss1unz1u301i6o \
     -e APP_SERVER_AUTH_USER=ModelLoader \
