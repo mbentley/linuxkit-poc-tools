43dd6876434a6a95503b0c8b58d5928ffc8a2a49
Matt Bentley <mbentley@mbentley.net>
2017-07-07 14:28:11 -0400

    Fix formatting
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- onap/sdc.sh
+++ onap/sdc.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-set -e 
+set -e
 
 remove() {
 	echo -e "\nKilling and removing containers..."
+        #shellcheck disable=2046
 	docker kill $(docker ps -f label=app=sdc -qa) || true
+        #shellcheck disable=2046
 	docker rm $(docker ps -f label=app=sdc -qa) || true
 
 	echo -e "\nRemoving volumes..."
+        #shellcheck disable=2046
 	docker volume rm $(docker volume ls -f label=app=sdc -q) || true
 
 	echo -e "\nRemoving networks..."
+        #shellcheck disable=2046
 	docker network rm $(docker network ls -f label=app=sdc -q) || true
 
 }
@@ -35,7 +39,7 @@ launch() {
 	  -e HOST_IP=172.31.4.207 \
 	  -e ES_HEAP_SIZE=1024M \
 	  -v /etc/localtime:/etc/localtime \
-	  -v ${HOME}/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
+	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
 	  -v sdc-es:/usr/share/elasticsearch/data \
 	  -v sdc-logs:/var/lib/jetty/logs \
 	  dtr.att.dckr.org/onap/sdc-elasticsearch:1.0-STAGING-latest
@@ -52,7 +56,7 @@ launch() {
 	  -e ES_HEAP_SIZE=1024M \
 	  -v /etc/localtime:/etc/localtime \
 	  -v /dockerdata-nfs/onapdemo/sdc/sdc-cs/CS:/var/lib/cassandra \
-	  -v ${HOME}/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
+	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
 	  -v sdc-logs:/var/lib/jetty/logs \
 	  dtr.att.dckr.org/onap/sdc-cassandra:1.0-STAGING-latest
 
@@ -65,7 +69,7 @@ launch() {
 	  -e ENVNAME=AUTO \
 	  -e ELASTICSEARCH_URL=http://sdc-es:9200 \
 	  -v /etc/localtime:/etc/localtime \
-	  -v ${HOME}/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
+	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
 	  -v sdc-logs:/var/lib/jetty/logs \
 	  dtr.att.dckr.org/onap/sdc-kibana:1.0-STAGING-latest
 
@@ -79,9 +83,9 @@ launch() {
 	  -e ENVNAME=AUTO \
 	  -e HOST_IP=172.31.4.207 \
 	  -v /etc/localtime:/etc/localtime \
-	  -v ${HOME}/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
-	  -v ${HOME}/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/jetty/keystore:/var/lib/jetty/etc/keystore \
-	  -v ${HOME}/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/sdc-fe/FE_2_setup_configuration.rb:/root/chef-solo/cookbooks/sdc-catalog-fe/recipes/FE_2_setup_configuration.rb \
+	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
+	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/jetty/keystore:/var/lib/jetty/etc/keystore \
+	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/sdc-fe/FE_2_setup_configuration.rb:/root/chef-solo/cookbooks/sdc-catalog-fe/recipes/FE_2_setup_configuration.rb \
 	  -v sdc-es:/usr/share/elasticsearch/data \
 	  -v sdc-logs:/var/lib/jetty/logs \
 	  dtr.att.dckr.org/onap/sdc-frontend:1.0-STAGING-latest
@@ -99,8 +103,8 @@ launch() {
 	  -e ENVNAME=AUTO \
 	  -e HOST_IP=172.31.4.207 \
 	  -v /etc/localtime:/etc/localtime \
-	  -v ${HOME}/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
-	  -v ${HOME}/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/jetty/keystore:/var/lib/jetty/etc/keystore \
+	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/environments:/root/chef-solo/environments \
+	  -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdc/jetty/keystore:/var/lib/jetty/etc/keystore \
 	  -v sdc-es:/usr/share/elasticsearch/data \
 	  -v sdc-logs:/var/lib/jetty/logs \
 	  dtr.att.dckr.org/onap/sdc-backend:1.0-STAGING-latest
