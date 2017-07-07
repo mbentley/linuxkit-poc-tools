345b2ba92fca076972dc191df6c3128d1198f05e
Matt Bentley <mbentley@mbentley.net>
2017-07-07 15:47:03 -0400

    Added all script to launch or remove all
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

---
+++ onap/_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+execute() {
+  for APP in ${APPS}
+  do
+    "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/"${APP}".sh "${1}"
+  done
+}
+
+main() {
+  case $1 in
+    launch)
+      APPS="aai sdc"
+      execute "${1}"
+      ;;
+    remove)
+      APPS="sdc aai"
+      execute "${1}"
+      ;;
+    *)
+      echo "Usage: $0 {launch|remove}"
+      exit 1
+      ;;
+  esac
+}
+
+main "${@}"
