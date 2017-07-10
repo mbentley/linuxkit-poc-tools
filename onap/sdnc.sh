55f53c0744b1b1d7155233e8c66fbcd2f66022c3
Matt Bentley <mbentley@mbentley.net>
2017-07-10 18:15:20 -0400

    Added sdnc
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

---
+++ onap/sdnc.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+set -e
+
+remove() {
+  echo -e "\nKilling and removing containers..."
+  #shellcheck disable=2046
+  docker kill $(docker ps -f label=app=sdnc -qa) || true
+  #shellcheck disable=2046
+  docker rm $(docker ps -f label=app=sdnc -qa) || true
+
+  echo -e "\nRemoving volumes..."
+  #shellcheck disable=2046
+  docker volume rm $(docker volume ls -f label=app=sdnc -q) || true
+
+  echo -e "\nRemoving networks..."
+  #shellcheck disable=2046
+  docker network rm $(docker network ls -f label=app=sdnc -q) || true
+}
+
+launch() {
+  # sdnc
+  echo -e "\nCreating network..."
+  docker network create --label app=sdnc --label onap=1 --driver bridge onap-sdnc
+
+  echo -e "\nCreating volumes..."
+  docker volume create --label app=sdnc --label onap=1 --driver local sdnc-data
+
+  ## sdnc-dbhost
+  echo -e "\nLaunching sdnc-dbhost..."
+  docker run -d --name sdnc-dbhost \
+    --label onap=1 \
+    --label app=sdnc \
+    --net onap-sdnc \
+    -p 3306 \
+    -e MYSQL_ROOT_PASSWORD="openECOMP1.0" \
+    -e MYSQL_ROOT_HOST='%' \
+    -v sdnc-data:/var/lib/mysql \
+    dtr.att.dckr.org/onap/mysql-server:5.6
+
+  ## sdnc-dgbuilder-container
+  echo -e "\nLaunching sdnc-dgbuilder-container..."
+  docker run -d --name sdnc-dgbuilder-container \
+    --label onap=1 \
+    --label app=sdnc \
+    --net onap-sdnc \
+    -p 3100 \
+    -e MYSQL_ROOT_PASSWORD="openECOMP1.0" \
+    -e SDNC_CONFIG_DIR=/opt/openecomp/sdnc/data/properties \
+    dtr.att.dckr.org/onap/dgbuilder-sdnc-image:1.0-STAGING-latest \
+      /bin/bash -c 'cd /opt/openecomp/sdnc/dgbuilder/ && ./start sdnc1.0 && wait'
+
+  ## sdnc
+  echo -e "\nLaunching sdnc-controller-container..."
+  docker run -d --name sdnc-controller-container \
+    --label onap=1 \
+    --label app=sdnc \
+    --net onap-sdnc \
+    -p 8181 \
+    -e MYSQL_ROOT_PASSWORD="openECOMP1.0" \
+    -e SDNC_CONFIG_DIR=/opt/openecomp/sdnc/data/properties \
+    -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdnc/conf:/opt/openecomp/sdnc/data/properties \
+    dtr.att.dckr.org/onap/sdnc-image:1.0-STAGING-latest \
+      /opt/openecomp/sdnc/bin/startODL.sh
+
+  ## sdnc-portal-container
+  echo -e "\nLaunching sdnc-portal-container..."
+  docker run -d --name sdnc-portal-container \
+    --label onap=1 \
+    --label app=sdnc \
+    --net onap-sdnc \
+    -p 8843 \
+    -e MYSQL_ROOT_PASSWORD="openECOMP1.0" \
+    -e SDNC_CONFIG_DIR=/opt/openecomp/sdnc/data/properties \
+    -v "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/sdnc/conf:/opt/openecomp/sdnc/data/properties \
+    dtr.att.dckr.org/onap/admportal-sdnc-image:1.0-STAGING-latest \
+      /bin/bash -c 'cd /opt/openecomp/sdnc/admportal/shell && ./start_portal.sh'
+}
+
+main() {
+  case $1 in
+    launch|remove)
+      ${1}
+      ;;
+    *)
+      echo "Usage: $0 {launch|remove}"
+      exit 1
+      ;;
+  esac
+}
+
+main "${@}"
