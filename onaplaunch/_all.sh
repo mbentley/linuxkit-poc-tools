309f4272fb864f3b441380859fb965016aad9528
Matt Bentley <matt.bentley@docker.com>
2017-08-11 10:51:51 -0400

    Added setting of IP in config
    Signed-off-by: Matt Bentley <matt.bentley@docker.com>

--- onaplaunch/_all.sh
+++ onaplaunch/_all.sh
@@ -16,6 +16,12 @@ clone() {
   then
     docker run --rm -v /root:/root dtr.att.dckr.org/services/oomclone:latest
   fi
+
+  # fix static ip in config
+  DEFAULT_IFACE=$(awk '$2 == 00000000 { print $1 }' /proc/net/route)
+  DEFAULT_IP="$(ip addr show dev "${DEFAULT_IFACE}" | awk '$1 == "inet" { sub("/.*", "", $2); print $2 }')"
+  echo "${DEFAULT_IP}" > "${HOME}"/git/gerrit.onap.org/oom/kubernetes/config/docker/init/src/config/policy/opt/policy/config/pe/ip_ad
+  dr.txt
 }
 
 main() {
